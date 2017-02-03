373e9b8a9486fe2458ec77ed0da82764ba0ae086
Alejandro Galvan <agalvanz@cisco.com>
2017-02-03 10:46:45 -0500

    Fixing bug, properly pretty printing

--- src/main/java/stealthwatch/flowfowarder/client/ExtFlowFunctions.java
+++ src/main/java/stealthwatch/flowfowarder/client/ExtFlowFunctions.java
@@ -5,15 +5,15 @@ import com.lancope.sw.ExternalFlowProtos.ExtFlow;
 import stealthwatch.flowfowarder.client.FieldFormatter.FieldFormatterType;
 
 public class ExtFlowFunctions {
+	
     public static String fromFlowExtToString(ExtFlow extFlow) {
-    	// TODO, not properly tested as when I was running the client late I wouldn't get any message
     	MessageFormatter formatter = new MessageFormatter(extFlow);
     	formatter.addNameFormatter("start_active_usec", FieldFormatterType.DATE);
     	formatter.addNameFormatter("last_active_usec", FieldFormatterType.DATE);
     	formatter.addTypeFormatter(".com.lancope.sw.IPAddress", FieldFormatterType.IP);
     	formatter.addTypeFormatter(".com.lancope.sw.MacAddress", FieldFormatterType.MAC);
     	String tmp = formatter.format();
-    	System.out.println(tmp);
     	return tmp;
     }
+
 }
--- src/main/java/stealthwatch/flowfowarder/client/MessageFormatter.java
+++ src/main/java/stealthwatch/flowfowarder/client/MessageFormatter.java
@@ -4,15 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.google.protobuf.DescriptorProtos.FieldDescriptorProto;
+import com.google.protobuf.DescriptorProtos.FieldDescriptorProto.Type;
+import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.GeneratedMessage;
 import com.lancope.sw.AddressProtos.IPAddress;
 import com.lancope.sw.AddressProtos.MacAddress;
 
 import stealthwatch.flowfowarder.client.FieldFormatter.FieldFormatterType;
-
-import com.google.protobuf.DescriptorProtos.FieldDescriptorProto;
-import com.google.protobuf.Descriptors.FieldDescriptor;
-import com.google.protobuf.DescriptorProtos.FieldDescriptorProto.Type;
 
 /**
  * The Message Formatter.
@@ -24,7 +23,7 @@ public class MessageFormatter {
 
 	/** The message to format. */
 	private GeneratedMessage message;
-	
+
 	/** The separator for each field. */
 	private String separator = "|";
 
@@ -33,15 +32,20 @@ public class MessageFormatter {
 
 	/** The field formatters filtered by name. */
 	private Map<String, FieldFormatterType> nameFieldFormatters = new HashMap<String, FieldFormatterType>();
-	
+
 	/** The field formatters filtered by type. */
 	private Map<String, FieldFormatterType> typeFieldFormatters = new HashMap<String, FieldFormatterType>();
-	
+
+	/** The suffix. */
+	private String suffix;
+
 	/**
 	 * Instantiates a new message formatter.
 	 *
-	 * @param <M> the generic type
-	 * @param message the message
+	 * @param <M>
+	 *            the generic type
+	 * @param message
+	 *            the message
 	 */
 	public <M extends GeneratedMessage> MessageFormatter(M message) {
 		this(message, null);
@@ -50,9 +54,12 @@ public class MessageFormatter {
 	/**
 	 * Instantiates a new message formatter.
 	 *
-	 * @param <M> the generic type
-	 * @param message the message
-	 * @param separator the separator
+	 * @param <M>
+	 *            the generic type
+	 * @param message
+	 *            the message
+	 * @param separator
+	 *            the separator
 	 */
 	public <M extends GeneratedMessage> MessageFormatter(M message, String separator) {
 		this(message, separator, null);
@@ -61,10 +68,14 @@ public class MessageFormatter {
 	/**
 	 * Instantiates a new message formatter.
 	 *
-	 * @param <M> the generic type
-	 * @param message the message
-	 * @param separator the separator
-	 * @param prefix the prefix
+	 * @param <M>
+	 *            the generic type
+	 * @param message
+	 *            the message
+	 * @param separator
+	 *            the separator
+	 * @param prefix
+	 *            the prefix
 	 */
 	public <M extends GeneratedMessage> MessageFormatter(M message, String separator, String prefix) {
 		super();
@@ -72,30 +83,42 @@ public class MessageFormatter {
 		setSeparator(separator);
 		setPrefix(prefix);
 	}
-	
+
 	/**
 	 * Instantiates a new message formatter.
 	 *
-	 * @param <M> the generic type
-	 * @param message the message
-	 * @param separator the separator
-	 * @param prefix the prefix
-	 * @param nameFieldFormatters the name field formatters
-	 * @param typeFieldFormatters the type field formatters
+	 * @param <M>
+	 *            the generic type
+	 * @param message
+	 *            the message
+	 * @param separator
+	 *            the separator
+	 * @param prefix
+	 *            the prefix
+	 * @param nameFieldFormatters
+	 *            the name field formatters
+	 * @param typeFieldFormatters
+	 *            the type field formatters
+	 * @param suffix
+	 *            the suffix
 	 */
-	private <M extends GeneratedMessage> MessageFormatter(M message, String separator, String prefix, Map<String, FieldFormatterType> nameFieldFormatters, Map<String, FieldFormatterType> typeFieldFormatters) {
+	private <M extends GeneratedMessage> MessageFormatter(M message, String separator, String prefix,
+			Map<String, FieldFormatterType> nameFieldFormatters, Map<String, FieldFormatterType> typeFieldFormatters,
+			String suffix) {
 		super();
 		this.message = message;
 		setSeparator(separator);
 		setPrefix(prefix);
 		this.nameFieldFormatters = nameFieldFormatters;
 		this.typeFieldFormatters = typeFieldFormatters;
+		this.suffix = suffix;
 	}
 
 	/**
 	 * Sets the separator.
 	 *
-	 * @param separator the new separator
+	 * @param separator
+	 *            the new separator
 	 */
 	public void setSeparator(String separator) {
 		if (separator != null) {
@@ -106,7 +129,8 @@ public class MessageFormatter {
 	/**
 	 * Sets the prefix.
 	 *
-	 * @param prefix the new prefix
+	 * @param prefix
+	 *            the new prefix
 	 */
 	public void setPrefix(String prefix) {
 		this.prefix = prefix;
@@ -115,18 +139,22 @@ public class MessageFormatter {
 	/**
 	 * Adds the name formatter.
 	 *
-	 * @param name the name
-	 * @param formatterType the formatter type
+	 * @param name
+	 *            the name
+	 * @param formatterType
+	 *            the formatter type
 	 */
 	public void addNameFormatter(String name, FieldFormatterType formatterType) {
 		nameFieldFormatters.put(name, formatterType);
 	}
-	
+
 	/**
 	 * Adds the type formatter.
 	 *
-	 * @param type the type
-	 * @param formatterType the formatter type
+	 * @param type
+	 *            the type
+	 * @param formatterType
+	 *            the formatter type
 	 */
 	public void addTypeFormatter(String type, FieldFormatterType formatterType) {
 		typeFieldFormatters.put(type, formatterType);
@@ -139,60 +167,123 @@ public class MessageFormatter {
 	 */
 	public String format() {
 		StringBuffer result = new StringBuffer("");
-    	Map<FieldDescriptor, Object> fields = message.getAllFields();
-    	for (Entry<FieldDescriptor, Object> entry : fields.entrySet()) {
-    		FieldDescriptorProto desc = entry.getKey().toProto();
-    		Object valueObj = entry.getValue();
-    		String name = (prefix == null ? "" : prefix + "-") + desc.getName();
-    		if (result.length() > 0) {
-    			result.append(separator);
-    		}
-    		if (desc.getType() != Type.TYPE_MESSAGE) {
-    			String value = null;
-    			FieldFormatterType type = nameFieldFormatters.get(name);
-    			if (type != null) {
-    				value = formatByType(type, valueObj);
-    			} else {
-    				value = valueObj.toString();
-    			}
-    			result.append(name).append(EQUALS).append(value);
-    		} else {
-    			FieldFormatterType type = typeFieldFormatters.get(desc.getTypeName());
-    			if (type != null) {
-    				result.append(name).append(EQUALS).append(formatByType(type, valueObj));
-    			} else {
-    				MessageFormatter innerFormat = new MessageFormatter((GeneratedMessage) valueObj, separator, name, nameFieldFormatters, typeFieldFormatters);
-    				result.append(innerFormat.format());
-    			}
-    		}
-    		
-    	}
+		Map<FieldDescriptor, Object> fields = message.getAllFields();
+		for (Entry<FieldDescriptor, Object> entry : fields.entrySet()) {
+			FieldDescriptor field = entry.getKey();
+			FieldDescriptorProto desc = entry.getKey().toProto();
+			Object valueObj = entry.getValue();
+			String name = (prefix == null ? "" : prefix + "-") + desc.getName() + (suffix == null ? "" : "-" + suffix);
+			if (result.length() > 0) {
+				result.append(separator);
+			}
+			if (desc.getType() != Type.TYPE_MESSAGE) {
+				formatSimpleField(result, valueObj, name);
+			} else {
+				formatNestedMessage(result, field, desc, valueObj, name);
+			}
+
+		}
 		return result.toString();
+	}
+
+	/**
+	 * Format simple field.
+	 *
+	 * @param result
+	 *            the result
+	 * @param valueObj
+	 *            the value obj
+	 * @param name
+	 *            the name
+	 */
+	private void formatSimpleField(StringBuffer result, Object valueObj, String name) {
+		String value = null;
+		FieldFormatterType type = nameFieldFormatters.get(name);
+		if (type != null) {
+			value = formatByType(type, valueObj);
+		} else {
+			value = valueObj.toString();
+		}
+		result.append(name).append(EQUALS).append(value);
+	}
+
+	/**
+	 * Format nested message.
+	 *
+	 * @param result
+	 *            the result
+	 * @param field
+	 *            the field
+	 * @param desc
+	 *            the desc
+	 * @param valueObj
+	 *            the value obj
+	 * @param name
+	 *            the name
+	 */
+	private void formatNestedMessage(StringBuffer result, FieldDescriptor field, FieldDescriptorProto desc,
+			Object valueObj, String name) {
+		FieldFormatterType type = typeFieldFormatters.get(desc.getTypeName());
+		if (type != null) {
+			result.append(name).append(EQUALS).append(formatByType(type, valueObj));
+		} else {
+			if (field.isRepeated()) {
+				formatRepeatedObject(result, valueObj, name);
+			} else {
+				MessageFormatter innerFormat = new MessageFormatter((GeneratedMessage) valueObj, separator, name,
+						nameFieldFormatters, typeFieldFormatters, null);
+				result.append(innerFormat.format());
+			}
+		}
+	}
+
+	/**
+	 * Format repeated object.
+	 *
+	 * @param result
+	 *            the result
+	 * @param valueObj
+	 *            the value obj
+	 * @param name
+	 *            the name
+	 */
+	private void formatRepeatedObject(StringBuffer result, Object valueObj, String name) {
+		int idx = 0;
+		for (Object val : ((Iterable<?>) valueObj)) {
+			MessageFormatter innerFormat = new MessageFormatter((GeneratedMessage) val, separator, name,
+					nameFieldFormatters, typeFieldFormatters, "" + idx);
+			if (idx > 0) {
+				result.append(separator);
+			}
+			result.append(innerFormat.format());
+			idx++;
+		}
 	}
 
 	/**
 	 * Format by type.
 	 *
-	 * @param type the type
-	 * @param val the val
+	 * @param type
+	 *            the type
+	 * @param val
+	 *            the val
 	 * @return the string
 	 */
 	private String formatByType(FieldFormatterType type, Object val) {
-		// TODO to test
 		String result = null;
 		switch (type) {
-			case IP :
-				IPAddress ip = (IPAddress) val;
-				result = FieldFormatter.formatIPAddress(ip.getA0(), ip.getA1());
-				break;
-			case MAC :
-				MacAddress mac = (MacAddress) val;
-				result = FieldFormatter.formatMacAddress(mac.getA());
-				break;
-			default : // DATE
-				Long microsecs = (Long) val;
-				result = FieldFormatter.formatDateFromMicroseconds(microsecs);
-				break;
+		case IP:
+			IPAddress ip = (IPAddress) val;
+			result = FieldFormatter.formatIPAddress(ip.getA0(), ip.getA1());
+			break;
+		case MAC:
+			MacAddress mac = (MacAddress) val;
+			result = FieldFormatter.formatMacAddress(mac.getA());
+			break;
+		default: // DATE
+			Long microsecs = (Long) val;
+			result = FieldFormatter.formatDateFromMicroseconds(microsecs);
+			break;
 		}
 		return result;
 	}
