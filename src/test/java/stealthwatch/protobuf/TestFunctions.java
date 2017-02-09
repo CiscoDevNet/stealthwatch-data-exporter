22d9e166ae13f5a1bea56dfb87ff63a8c7aa16e8
Tom Gagnier <tomgagnier@gmail.com>
2017-02-09 08:28:22 -0500

    Unit test for formatter.

--- src/test/java/stealthwatch/protobuf/TestFunctions.java
+++ src/test/java/stealthwatch/protobuf/TestFunctions.java
@@ -1,5 +1,7 @@
 package stealthwatch.protobuf;
 
+import java.util.Calendar;
+
 interface TestFunctions {
 
     static byte[] bytes(int... bytes) {
@@ -8,5 +10,17 @@ interface TestFunctions {
             result[i] = (byte) bytes[i];
         }
         return result;
+    }
+
+    @SuppressWarnings("MethodWithTooManyParameters")
+    static long epocMicroseconds(int year, int month, int date, int hour, int minute,
+                                 int second, int milliseconds, int useconds) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, date, hour, minute, second);
+        long time = calendar.getTime().getTime();
+        time += milliseconds;
+        time *= 1000;
+        time += useconds;
+        return time;
     }
 }
